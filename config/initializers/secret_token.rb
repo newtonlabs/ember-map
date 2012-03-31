f4560f34b0a5ca3c5546d12c43a97157c637f57e
Thomas Newton <tnewton@gmail.com>
2012-03-31 13:30:05 -0400

    rails as the IDE

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Embermap::Application.config.secret_token = '1dcf236852559bae3abdda949786489d91319c6333df312c01b28b57c6a21494bece2cefafd52a0ae07ace708b4ac7a21a59cb38a84201e51618bbb254cd0328'
