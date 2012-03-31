39eeb0ec05feb901dd9836f64f431bc5309c5c29
Thomas Newton <tnewton@gmail.com>
2012-03-31 15:00:31 -0400

    cleaning up generator mayhem

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,8 +1,4 @@
 Rails.application.config.generators do |g|
-    g.fixture_replacement :factory_girl, :dir => "spec/factories"
-    g.test_framework :rspec, :fixture => true
-    g.view_specs false
-
     g.template_engine :haml
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Embermap::Application.routes.draw do
+  get "map/index"
+
+  get "maps/index"
+
   root :to => "home#index"
 end
