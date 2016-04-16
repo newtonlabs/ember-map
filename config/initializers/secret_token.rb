d59644b24b57545542169e729b9564fd6bb2e03a
Thomas Newton <thomas_newton@mckinsey.com>
2016-04-16 08:18:40 -0400

    cleanup

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,3 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Embermap::Application.config.secret_token = '8ed39d724771447cd5b4c47825185d985e5b7b29da408a6d85a69d34bf9e591a2e2c1eb253e67ac3927a529c1135bb5a4128d7bccfc819df88d1c12ad36a222e'
